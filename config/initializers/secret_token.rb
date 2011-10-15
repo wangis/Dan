b32f42fe6794a7bb3c2283a3b11dba25c42f6b38
Owen Wang <wangfowen@gmail.com>
2011-10-15 16:46:48 -0400

    first commit. menu set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dan::Application.config.secret_token = 'e48877eec1d9b00eaf1567122fb3c632efac465eb56f38ebbef09172b9543a213d56b87b39b173e5c2411c8eec4818ae1cc0c40490cf2e04f0fbaf0f4568bcf6'
